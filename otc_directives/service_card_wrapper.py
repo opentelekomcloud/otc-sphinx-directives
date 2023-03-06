beb84dacde5b35112076d1cc59fb93f1d301fa7e
Sebastian Gode <sebastian.gode@telekom.de>
2023-03-06 10:13:29 +0000

    test1

--- otc_directives/service_card_wrapper.py
+++ otc_directives/service_card_wrapper.py
@@ -68,6 +68,8 @@ def service_card_wrapper_html(self, node):
     data = f"""
         <div class='muh'>
         """
+
+    print(node.content)
     data += node.content
     data += f"""
         </div>
