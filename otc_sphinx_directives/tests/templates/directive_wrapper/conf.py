1759f6981b9314c4f5f28b0f056a1eacec539908
tischrei <tino.schreiber@t-systems.com>
2023-03-22 10:49:42 +0000

    add first tests

---
+++ otc_sphinx_directives/tests/templates/directive_wrapper/conf.py
@@ -0,0 +1,41 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# !!!
+# This file is generated out of template in doc-exports repository.
+# Beware overwriting it locally.
+
+extensions = [
+    'otcdocstheme',
+    'otc_sphinx_directives'
+]
+
+otcdocs_auto_name = False
+otcdocs_auto_version = False
+
+source_suffix = '.rst'
+source_encoding = 'utf-8'
+master_doc = 'index'
+html_theme = 'otcdocs'
+html_theme_options = {
+}
+html_title = "Test Case"
+html_static_path = ['_static']
+html_copy_source = False
+
+# -- Options for PDF output --------------------------------------------------
+# latex_documents = [
+#     ('index',
+#      'ecs-umn.tex',
+#      u'Elastic Cloud Server - User Guide',
+#      u'OpenTelekomCloud', 'manual'),
+# ]
