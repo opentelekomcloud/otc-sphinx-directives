31f1f4a12cefb7f3e7e5c1232290f43f26e6d425
tischrei <tino.schreiber@t-systems.com>
2024-06-12 09:26:27 +0000

    change existing wrappers

--- doc/source/examples/container_item.rst
+++ doc/source/examples/container_item.rst
@@ -1,11 +1,61 @@
 Container Item Test
 ===================
 
-.. container:: row row-cols-1 row-cols-md-3 g-4
+.. directive_wrapper::
+   :class: container_item_wrapper
+   :style: display: flex;flex-wrap: wrap;flex-direction: row;justify-content: flex-start;gap: 1.5rem;
 
-   .. container_item::
-      :title: Ansible
-      :image: _static/images/ansible.svg
-      :external:
+   .. directive_wrapper::
+      :class: container_item_size
+      :style: width:30%;
 
-      - Ansible Collection|https://docs.otc-service.com/ansible-collection-cloud
+      .. container_item::
+         :title: Ansible
+         :image: ../_static/images/ansible.svg
+         :description: Ansible is a suite of software tools that enables infrastructure as code. It is open-source and the suite includes software provisioning, configuration management, and application deployment functionality.
+
+         - Ansible Collection|https://docs.otc-service.com/ansible-collection-cloud
+      
+   .. directive_wrapper::
+      :class: container_item_size
+      :style: width:30%;
+
+      .. container_item::
+         :title: Ansible
+         :image: ../_static/images/ansible.svg
+         :description: Ansible is a suite of software tools bbbbbbbbbbbbbbbbbbbbbbbbb bbbbbbbbbbbbbb bbbbbbbbbbbbbbbbsssss fsdgg fsdfgsdf gdfsgfsdfg sdfgdsg sdfgd that enables infrastructure as code. It is open-source and the suite includes software provisioning, configuration management, and application deployment functionality.
+
+         - Ansible Collection|https://docs.otc-service.com/ansible-collection-cloud
+
+   .. directive_wrapper::
+      :class: container_item_size
+      :style: width:30%;
+
+      .. container_item::
+         :title: Ansible
+         :image: ../_static/images/ansible.svg
+         :description: Ansible is a suite of software tools that enables infrastructure as code. It is open-source and the suite includes software provisioning, configuration management, and application deployment functionality.
+
+         - Ansible Collection|https://docs.otc-service.com/ansible-collection-cloud
+
+   .. directive_wrapper::
+      :class: container_item_size
+      :style: width:30%;
+
+      .. container_item::
+         :title: Ansible
+         :image: ../_static/images/ansible.svg
+         :description: Ansible is a suite of software tools that enables infrastructure as code. It is open-source and the suite includes software provisioning, configuration management, and application deployment functionality.
+
+         - Ansible Collection|https://docs.otc-service.com/ansible-collection-cloud
+
+   .. directive_wrapper::
+      :class: container_item_size
+      :style: width:30%;
+
+      .. container_item::
+         :title: Ansible
+         :image: ../_static/images/ansible.svg
+         :description: Ansible is a suite of software tools that enables infrastructure as code. It is open-source and the suite includes software provisioning, configuration management, and application deployment functionality.
+
+         - Ansible Collection|https://docs.otc-service.com/ansible-collection-cloud
--- otc_sphinx_directives/container_item.py
+++ otc_sphinx_directives/container_item.py
@@ -50,20 +50,21 @@ METADATA = otc_metadata.services.Services()
 class ContainerItem(Directive):
     node_class = container_item
     option_spec = {
-        'title': directives.unchanged,
-        'image': directives.unchanged,
-        'external': directives.unchanged
+        'title': directives.unchanged_required,
+        'image': directives.unchanged_required,
+        'external': directives.unchanged,
+        'description': directives.unchanged_required,
     }
 
     has_content = True
 
     def run(self):
-        doctree_node = container_item()
-        doctree_node['title'] = self.options['title']
-        if 'image' in self.options:
-            doctree_node['image'] = self.options['image']
+        node = container_item()
+        node['title'] = self.options['title']
+        node['image'] = self.options['image']
+        node['description'] = self.options['description']
         # Check, if 'external' is available in self.options and set the value for the node
-        doctree_node['external'] = 'external' in self.options
+        node['external'] = 'external' in self.options
         services = []
         for ent in self.content:
             _srv = ent.strip('- ')
@@ -76,46 +77,67 @@ class ContainerItem(Directive):
                     href=href
                 )
             )
-        doctree_node['services'] = services
-        return [doctree_node]
+        node['services'] = services
+        return [node]
 
 
 def container_item_html(self, node):
-    tmpl = """
-        <div class="col">
-          <div class="card">
-            %(img)s
-            <div class="card-body">
-              <h5 class="card-title">%(title)s</h5>
-            </div>
-            %(data)s
-          </div>
-        </div>
-        """
 
-    if node['external']:
-        node['data'] = (
-            "<ul class='list-group list-group-flush'>"
-            + "".join([('<li class="list-group-item"><a href="%(href)s" target="_blank" rel="noopener noreferrer">'
-                        '<div class="col-md-10">%(title)s</div>'
-                        '</a></li>'
-                        % x)
-                      for x in node['services']])
-            + "</ul>")
-    else:
-        node['data'] = (
-            "<ul class='list-group list-group-flush'>"
-            + "".join([('<li class="list-group-item"><a href="%(href)s">'
-                        '<div class="col-md-10">%(title)s</div>'
-                        '</a></li>'
-                        % x)
-                      for x in node['services']])
-            + "</ul>")
-    node['img'] = ''
-    if 'image' in node and node['image']:
-        node['img'] = (
-            f'<img src="{node["image"]}" '
-            'class="card-img-top mx-auto">'
-        )
-    self.body.append(tmpl % node)
+    data = f'''
+        <scale-card>
+            <div style="margin: -1.5rem;display: flex;flex-wrap: wrap;justify-content: center;">
+            <img
+                style="margin: 0;display: flex;flex-wrap: wrap;justify-content: center;align-content: center;max-height: 160px;"
+                src="{node['image']}"
+                alt="{node['title']}"
+            />
+            <div style="padding: 1rem;">
+                <h4 style="margin: 0px 0 1rem 0; font: var(--telekom-text-style-heading-4);">{node['title']}</h4>
+                <span>{node['description']}</span>
+            </div>
+            </div>
+        </scale-card>
+    '''
+    self.body.append(data)
+
+
+
+
+    # tmpl = """
+    #     <div class="col">
+    #       <div class="card">
+    #         %(img)s
+    #         <div class="card-body">
+    #           <h5 class="card-title">%(title)s</h5>
+    #         </div>
+    #         %(data)s
+    #       </div>
+    #     </div>
+    #     """
+
+    # if node['external']:
+    #     node['data'] = (
+    #         "<ul class='list-group list-group-flush'>"
+    #         + "".join([('<li class="list-group-item"><a href="%(href)s" target="_blank" rel="noopener noreferrer">'
+    #                     '<div class="col-md-10">%(title)s</div>'
+    #                     '</a></li>'
+    #                     % x)
+    #                   for x in node['services']])
+    #         + "</ul>")
+    # else:
+    #     node['data'] = (
+    #         "<ul class='list-group list-group-flush'>"
+    #         + "".join([('<li class="list-group-item"><a href="%(href)s">'
+    #                     '<div class="col-md-10">%(title)s</div>'
+    #                     '</a></li>'
+    #                     % x)
+    #                   for x in node['services']])
+    #         + "</ul>")
+    # node['img'] = ''
+    # if 'image' in node and node['image']:
+    #     node['img'] = (
+    #         f'<img src="{node["image"]}" '
+    #         'class="card-img-top mx-auto">'
+    #     )
+    # self.body.append(tmpl % node)
     raise nodes.SkipNode
