db123c2b59e2d20e517e1b722aaab56e41ac5950
tischrei <tino.schreiber@t-systems.com>
2025-08-18 10:22:02 +0000

    push current results

--- otc_sphinx_directives/popular_services.py
+++ otc_sphinx_directives/popular_services.py
@@ -21,7 +21,7 @@ from otc_metadata.analytics.data import AnalyticsData
 import otc_metadata.services
 
 LOG = logging.getLogger(__name__)
-
+METADATA = otc_metadata.services.Services()
 
 class popular_services(nodes.General, nodes.Element):
     pass
@@ -42,16 +42,43 @@ class PopularServices(Directive):
         node['cloud_environment'] = cloud_env
         return [node]
 
-METADATA = otc_metadata.services.Services()
+
 
 def popular_services_html(self, node):
     # This method renders containers per each service of the category with all
     # links as individual list items
 
-    popular_services = json.dumps(METADATA.all_services_by_cloud_environment(cloud_environment=node['cloud_environment'], environments=['public']))
-    data = f'''
-        <div class="popular-services">Cloud Environment: {popular_services}</div>
-    '''
+    data = ''
+    analytics_data = AnalyticsData().analytics_data_by_cloud_environment(cloud_environment=node['cloud_environment'])
+    cloud_env_services = METADATA.all_services_by_cloud_environment_as_dict(cloud_environment=node['cloud_environment'], environments=['public'])
+    popular_services = []
+
+    for svc in analytics_data:
+        if svc in cloud_env_services:
+            popular_services.append(cloud_env_services[svc])
+    
+    for svc in popular_services:
+        data += f'''
+            <div class="card item-sbv item-sbv-flex">
+                <a href="/{svc['service_uri']}/">
+                    <div class="card-body">
+                        <h4>{svc['service_title']}</h4>
+        '''
+        if 'description' in svc:
+            data += f'''
+                <p>{svc['description']}</p>
+            '''
+
+        data += f'''
+                    </div>
+                </a>
+            </div>
+        '''
+
+    # popular_services = json.dumps(METADATA.all_services_by_cloud_environment_as_dict(cloud_environment=node['cloud_environment'], environments=['public']))
+    # data += f'''
+    #     <div class="popular-services">Cloud Environment: {popular_services}</div>
+    # '''
 
     self.body.append(data)
     raise nodes.SkipNode
