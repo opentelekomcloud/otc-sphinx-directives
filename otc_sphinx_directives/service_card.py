75be1566ab681756a46886a819ff827a1cf409fc
Sebastian Gode <sebastian.gode@telekom.de>
2025-10-13 12:03:44 +0000

    Remove check jobs and fix dev-guide

--- otc_sphinx_directives/service_card.py
+++ otc_sphinx_directives/service_card.py
@@ -142,6 +142,8 @@ def service_card_html(self, node):
                         if cloud["pdf_visibility"] == "hidden":
                             print("PDF not enabled anywhere!")
                         elif (cloud["pdf_visibility"] == "internal" and node['environment'] == "internal") or (cloud["pdf_visibility"] == "public"):
+                            if doc["type"] == "dev":
+                                doc["type"] = "dev-guide"
                             data += (f'''
                                         <scale-button variant="secondary" class="pdf-button-sbv" href="{node['service_type']}-{doc["type"]}.pdf" data-umami-event="PDF Download" data-umami-event-pdfname="{node['service_type']}-{doc["type"]}.pdf" target="_blank">
                                         <scale-icon-user-file-pdf-file accessibility-title="pdf-file"></scale-icon-user-file-pdf-file>
